e5e0ae953249462025e9c3ccec16ba9c8dc9b116
Reid Henderson <reid.henderson@gmail.com>
2017-06-15 17:37:03 -0400

    testing heroku env variables

--- server.js
+++ server.js
@@ -1,54 +1,51 @@
-
 // Dependencies
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
-const logger = require("morgan");
+// const path = require('path');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 // Mongo Models
-const Article = require('./models/article.js');
-const Comment = require('./models/comment.js')
+// const Article = require('./models/article.js');
+// const Comment = require('./models/comment.js');
 
-// Set up environment variables
+// Set up environment variables and database
 const PORT = process.env.PORT || 8080;
 const database = {
-  'development' : 'mongodb://localhost/news-scraper',
-  'production' : "mongodb://heroku_wwwj0xp6:f5d5p1mv3o9jq94lhui18tkvcv@ds149207.mlab.com:49207/heroku_wwwj0xp6",
+  development: 'mongodb://localhost/news-scraper',
+  production: 'mongodb://heroku_wwwj0xp6:f5d5p1mv3o9jq94lhui18tkvcv@ds149207.mlab.com:49207/heroku_wwwj0xp6',
 };
 
+console.log('production:', process.env.MONGODB_URI);
+
 const env = process.env.NODE_ENV || 'development';
+// Setup Mongoose and add promise model
+mongoose.Promise = Promise;
+mongoose.connect(database[env]);
+const db = mongoose.connection; // Save mongoose default connection
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on('error', (error) => {
+  console.error('Database Error:', error);
+});
 
 // Initialize express
 const app = express();
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static("public"));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 
-
-// Database configuration
-// Set mongoose promise model
-mongoose.Promise = Promise;
-mongoose.connect(database[env]);
-const db = mongoose.connection; //Save mongoose default connection
-
-
-// This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-const routes = require('./controllers/routes.js')(app);
+// Import routes from routes.js
+require('./controllers/routes.js')(app);
 
 // Connect to database and set the app to listen on port 3000
-db.once('open', function() {
-  console.log("Connected to database");
-  app.listen(PORT, function() {
-    console.log("App running on port", PORT);
+db.once('open', () => {
+  console.log('Connected to database');
+  app.listen(PORT, () => {
+    console.log('App running on port', PORT);
   });
 });
