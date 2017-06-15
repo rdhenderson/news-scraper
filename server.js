e258b86c3d618439ce4d384d41fbdc5ac2f610c7
Reid Henderson <reid.henderson@gmail.com>
2017-06-15 17:59:19 -0400

    heroku

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 // Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-// const path = require('path');
-// const logger = require('morgan');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
-
-// Mongo Models
-// const Article = require('./models/article.js');
-// const Comment = require('./models/comment.js');
 
 // Set up environment variables and database
 const PORT = process.env.PORT || 8080;
-const database = {
-  development: 'mongodb://localhost/news-scraper',
-  production: 'mongodb://heroku_wwwj0xp6:f5d5p1mv3o9jq94lhui18tkvcv@ds149207.mlab.com:49207/heroku_wwwj0xp6',
-};
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/news-scraper';
 
-console.log('production:', process.env.MONGODB_URI);
-console.log('production:', JSON.stringify(process.env));
-
-const env = process.env.NODE_ENV || 'development';
 // Setup Mongoose and add promise model
 mongoose.Promise = Promise;
-mongoose.connect(database[env]);
+mongoose.connect(MONGODB_URI);
 const db = mongoose.connection; // Save mongoose default connection
 // This makes sure that any errors are logged if mongodb runs into an issue
 db.on('error', (error) => {
@@ -31,7 +19,7 @@ db.on('error', (error) => {
 
 // Initialize express
 const app = express();
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
